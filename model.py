e855d633e5b24aec56c1932af9590b23556efdda
fabioscap <fabiovt_99@hotmail.it>
2022-06-27 19:14:04 +0200

    added small model

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from torchvision.models import resnet
 from torchvision.models.resnet import BasicBlock
 
 # returns a convolution block with batch norm
-def convolution_block(in_channels, out_channels, ksize, stride, padding = 1,bias=True):
+def convolution_block(in_channels, out_channels, ksize = 3, stride = 1, padding = 1,bias=True):
     return nn.Sequential(
         nn.Conv2d(in_channels = in_channels,
                   out_channels = out_channels,
@@ -263,4 +263,103 @@ class Net2(nn.Module):
         
         return y
 
+# replace resBlocks with standard convolution blocks
+class Net3(nn.Module):
+    def __init__(self,shape=(32,64,128,256)):
+        super().__init__()
+        self.rgb_conv1 = convolution_block(in_channels=3,
+                                   out_channels=shape[0]*3//4,
+                                   ksize=3,
+                                   stride=1,
+                                   padding=1)
 
+        self.d_conv1 = convolution_block(in_channels=1,
+                                        out_channels=shape[0]//4,
+                                        ksize=3,
+                                        stride=1,
+                                        padding=1)
+
+        self.conv2 = convolution_block(in_channels=shape[0],
+                                       out_channels=shape[0])
+
+        self.conv3 = convolution_block(in_channels=shape[0],out_channels=shape[1],stride=2)
+
+        self.conv4 = convolution_block(in_channels=shape[1],out_channels=shape[2],stride=2)
+
+        self.conv5 = convolution_block(in_channels=shape[2],out_channels=shape[3],stride=2)
+
+        self.conv6 = convolution_block(in_channels = shape[3],
+                                        out_channels = shape[3],
+                                        ksize = 3,
+                                        stride = 2,
+                                        padding = 1)
+        self.convt5 = upconvolution_block(in_channels = shape[3],
+                                          out_channels = shape[2],
+                                          ksize = 3,
+                                          stride = 2,
+                                          padding = 1,
+                                          out_padding = 1)
+        self.convt4 = upconvolution_block(in_channels = shape[3]+shape[2], # 384
+                                          out_channels = shape[1],
+                                          ksize = 3,
+                                          stride = 2,
+                                          padding = 1,
+                                          out_padding = 1)
+        self.convt3 = upconvolution_block(in_channels = shape[2]+shape[1], # 192
+                                          out_channels = shape[0],
+                                          ksize = 3,
+                                          stride = 2,
+                                          padding = 1,
+                                          out_padding = 1)
+        self.convt2 = upconvolution_block(in_channels = shape[1]+shape[0], # 96
+                                          out_channels = shape[0],
+                                          ksize = 3,
+                                          stride = 2,
+                                          out_padding = 1)
+        self.convt1 = upconvolution_block(in_channels = shape[1],
+                                          out_channels = shape[0],
+                                          ksize = 3,
+                                          stride = 1,
+                                          out_padding = 0)
+
+        self.convtf = convolution_block(in_channels = shape[1],
+                                        out_channels = 1,
+                                        ksize = 1,
+                                        stride = 1,
+                                        padding = 0)
+
+        self.relu = nn.ReLU()
+
+
+    def forward(self,rgb,depth):
+        a = self.relu(self.rgb_conv1(rgb))
+        b = self.relu(self.d_conv1(depth))
+        
+        x = torch.cat((a,b),1)
+
+        # encoder
+        conv2 = self.conv2(x)
+        conv3 = self.conv3(conv2)
+        conv4 = self.conv4(conv3)
+        conv5 = self.conv5(conv4)
+        conv6 = self.relu(self.conv6(conv5))
+
+        # decoder
+        convt5 = self.relu(self.convt5(conv6))
+        y = torch.cat((convt5, conv5), 1)
+
+        convt4 = self.relu(self.convt4(y))
+        y = torch.cat((convt4, conv4), 1)
+
+        convt3 = self.relu(self.convt3(y))
+        y = torch.cat((convt3, conv3), 1)
+
+        convt2 = self.relu(self.convt2(y))
+        y = torch.cat((convt2, conv2), 1)
+
+        convt1 = self.relu(self.convt1(y))
+        y = torch.cat((convt1, x), 1)
+
+        y = self.convtf(y)
+        
+        return y
